36e4c5be8692fbe8b8e5b51fc3aa24ada652ddf3
Joshua Rymer <joshuarymer1@gmail.com>
2022-11-01 10:26:03 +0000

    Create operate function

--- script.js
+++ script.js
@@ -6,10 +6,29 @@ function subtract(a, b) {
     return a - b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
+
 function divide(a, b) {
     return a / b;
 }
 
-function multiply(a, b) {
-    return a * b;
+function operate(op, a, b) {
+    switch(op) {
+        case add:
+            return add(a, b);
+            break;
+        case subtract:
+            return subtract(a, b);
+            break;
+        case multiply:
+            return multiply(a, b);
+            break;
+        case divide:
+            return divide(a, b);
+            break;
+    }
 }
+
